794fa553f746abd019459375df5fcd7034affb55
Jim Kamans <j.b.m.kamans@hva.nl>
2021-12-07 21:55:29 +0100

    [day02-p1] Improve code by using .each_with_object

--- ruby/day02/day02_part1.rb
+++ ruby/day02/day02_part1.rb
@@ -3,14 +3,10 @@
 require '../utils/aoc_input_file'
 inputfile = AOCInputFile.path(__FILE__)
 
-depths = File.readlines(inputfile)
-
-n = 0
-instructions = { 'forward' => 0, 'up' => 0, 'down' => 0 }
-while n < depths.length
-  instruction, value = depths[n].split
-  instructions[instruction] += value.to_i
-  n += 1
-end
-
-p instructions['forward'] * (instructions['down'] - instructions['up'])
+instruction_to_sum = File.readlines(inputfile)
+                         .map(&:split)
+                         .each_with_object({ 'forward' => 0, 'up' => 0, 'down' => 0 }) do |line, obj|
+                           obj[line[0]] += line[1].to_i
+                         end
+p instruction_to_sum
+p instruction_to_sum['forward'] * (instruction_to_sum['down'] - instruction_to_sum['up'])
