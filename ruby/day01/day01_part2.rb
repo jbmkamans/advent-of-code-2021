d406515fcbca4de533c0e26b88380cad0574553c
Jim Kamans <j.b.m.kamans@hva.nl>
2021-12-07 20:32:07 +0100

    [day01-p2] Improve code by smart enumeration

--- ruby/day01/day01_part2.rb
+++ ruby/day01/day01_part2.rb
@@ -6,12 +6,5 @@ inputfile = AOCInputFile.path(__FILE__)
 depths = File.readlines(inputfile).map(&:to_i)
 p depths
 
-depth_diffs = []
-n = 0
-
-while n < depths.length - 3
-  depth_diffs << 1 if depths[n] < depths[n + 3]
-  n += 1
-end
-
-p depth_diffs.reduce(&:+)
+ans = depths.each_cons(4).map { |first, _, _, fourth| fourth > first }.count(true)
+puts "\nThe answer is: #{ans}"
