469f81d9646ddae6ba01576b91cf501da614a321
Jim Kamans <j.b.m.kamans@hva.nl>
2021-12-07 20:23:20 +0100

    [day01-p1] Improve code by using smart iteration

--- ruby/day01/day01_part1.rb
+++ ruby/day01/day01_part1.rb
@@ -6,12 +6,7 @@ inputfile = AOCInputFile.path(__FILE__)
 depths = File.readlines(inputfile).map(&:to_i)
 p depths
 
-# map each index to true if its successor is greater, false otherwise
-depths.map!.with_index do |depth, index|
-  # skip last index, that has no successor
-  next if index >= depths.size - 1
-
-  depth < depths[index + 1]
-end
-
-puts "\nThe answer is: #{depths.count(true)}"
+# map each two consecutive numbers to true if the second number is greater than the first number,
+# then count the number of true's.
+ans = depths.each_cons(2).map { |first, second| second > first }.count(true)
+puts "\nThe answer is: #{ans}"
